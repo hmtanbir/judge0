cd97a17769177757641238c7690a8e17c027bc21
HM Tanbir <tanbir2025@gmail.com>
2025-12-20 20:51:57 +0600

    revert some config

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,9 @@
-# Fetch environment variables with defaults
-threads_count  = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))      # default 5 threads
-process_count  = Integer(ENV.fetch('RAILS_SERVER_PROCESSES', 2)) # default 2 workers
-port           = Integer(ENV.fetch('PORT', 2358))                # default port 2358
-rails_env      = ENV.fetch('RAILS_ENV', 'development')          # default development
-
-# Puma configuration
+threads_count = Integer(ENV['RAILS_MAX_THREADS'])
+process_count = Integer(ENV['RAILS_SERVER_PROCESSES'])
 threads threads_count, threads_count
 workers process_count
 
-port port
-environment rails_env
+port ENV['PORT']
+environment ENV['RAILS_ENV']
 
 plugin :tmp_restart
