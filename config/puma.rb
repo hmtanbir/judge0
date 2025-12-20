e91835d4fd9488d8e1b642168285bd53d78f8b37
HM Tanbir <tanbir2025@gmail.com>
2025-12-20 19:12:01 +0600

    add default values in puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,14 @@
-threads_count = Integer(ENV['RAILS_MAX_THREADS'])
-process_count = Integer(ENV['RAILS_SERVER_PROCESSES'])
+# Fetch environment variables with defaults
+threads_count  = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))      # default 5 threads
+process_count  = Integer(ENV.fetch('RAILS_SERVER_PROCESSES', 2)) # default 2 workers
+port           = Integer(ENV.fetch('PORT', 2358))                # default port 2358
+rails_env      = ENV.fetch('RAILS_ENV', 'development')          # default development
+
+# Puma configuration
 threads threads_count, threads_count
 workers process_count
 
-port ENV['PORT']
-environment ENV['RAILS_ENV']
+port port
+environment rails_env
 
 plugin :tmp_restart
